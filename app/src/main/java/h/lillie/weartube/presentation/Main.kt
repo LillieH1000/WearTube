c4db0cd9df1002adc2d6436d5b446e55477b7d0b
Lillie <91358136+LillieH1000@users.noreply.github.com>
2023-12-27 16:36:35 -0500

    Improve search ui

--- app/src/main/java/h/lillie/weartube/presentation/Main.kt
+++ app/src/main/java/h/lillie/weartube/presentation/Main.kt
@@ -10,11 +10,14 @@ import android.view.KeyEvent
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.RelativeLayout
 import android.widget.ScrollView
+import android.widget.Space
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import coil.load
 import h.lillie.weartube.R
 import org.json.JSONArray
 import org.json.JSONException
@@ -47,12 +50,20 @@ class Main : AppCompatActivity() {
                     try {
                         val videoID: String = searchContents.getJSONObject(i).getJSONObject("videoRenderer").optString("videoId")
                         val videoTitle: String = searchContents.getJSONObject(i).getJSONObject("videoRenderer").getJSONObject("title").getJSONArray("runs").getJSONObject(0).optString("text")
+                        val videoArtworkArray: JSONArray = searchContents.getJSONObject(i).getJSONObject("videoRenderer").getJSONObject("thumbnail").getJSONArray("thumbnails")
+                        val videoArtworkUrl: String = videoArtworkArray.getJSONObject((videoArtworkArray.length() - 1)).optString("url")
 
                         val videoRelativeView = RelativeLayout(this@Main)
-                        videoRelativeView.layoutParams = RelativeLayout.LayoutParams(deviceWidth, 200)
+                        videoRelativeView.layoutParams = RelativeLayout.LayoutParams(deviceWidth, 100)
+
+                        val videoImageView = ImageView(this@Main)
+                        videoImageView.layoutParams = LinearLayout.LayoutParams(110, 100)
+                        videoImageView.scaleType = ImageView.ScaleType.FIT_XY
+                        videoImageView.load(videoArtworkUrl)
 
                         val videoTitleTextView = TextView(this@Main)
-                        videoTitleTextView.layoutParams = LinearLayout.LayoutParams(deviceWidth, 160)
+                        videoTitleTextView.layoutParams = LinearLayout.LayoutParams(deviceWidth - 120, 100)
+                        videoTitleTextView.x = 120f
                         videoTitleTextView.text = videoTitle
                         videoTitleTextView.gravity = Gravity.CENTER_VERTICAL
 
@@ -65,9 +76,14 @@ class Main : AppCompatActivity() {
                             startActivity(intent)
                         }
 
+                        videoRelativeView.addView(videoImageView)
                         videoRelativeView.addView(videoTitleTextView)
                         videoRelativeView.addView(videoButton)
                         searchScrollLayout.addView(videoRelativeView)
+
+                        val spaceView = Space(this@Main)
+                        spaceView.minimumHeight = 4
+                        searchScrollLayout.addView(spaceView)
                     } catch (e: JSONException) {
                         Log.e("JSONException", e.toString())
                     }

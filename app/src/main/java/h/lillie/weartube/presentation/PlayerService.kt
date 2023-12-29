b21d6e40c5c5be557bd28061ac64e3eeff91c499
Lillie <91358136+LillieH1000@users.noreply.github.com>
2023-12-29 04:51:54 -0500

    Add forwardingplayer and callbacks to player service

--- app/src/main/java/h/lillie/weartube/presentation/PlayerService.kt
+++ app/src/main/java/h/lillie/weartube/presentation/PlayerService.kt
@@ -3,9 +3,11 @@ package h.lillie.weartube.presentation
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.net.Uri
+import androidx.media3.common.ForwardingPlayer
 import androidx.media3.common.MediaItem
 import androidx.media3.common.MediaMetadata
 import androidx.media3.common.MimeTypes
+import androidx.media3.common.Player
 import androidx.media3.datasource.okhttp.OkHttpDataSource
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.exoplayer.DefaultRenderersFactory
@@ -19,9 +21,10 @@ import java.util.concurrent.TimeUnit
 import okhttp3.OkHttpClient
 
 @SuppressLint("UnsafeOptInUsageError")
-class PlayerService : MediaSessionService() {
+class PlayerService : MediaSessionService(), Player.Listener, MediaSession.Callback {
     private lateinit var videoData: Data
     private lateinit var exoPlayer: ExoPlayer
+    private lateinit var forwardingExoPlayer: ForwardingPlayer
     private var playerSession: MediaSession? = null
 
     override fun onCreate() {
@@ -40,6 +43,8 @@ class PlayerService : MediaSessionService() {
             .setSeekForwardIncrementMs(TimeUnit.SECONDS.toMillis(10))
             .build()
 
+        exoPlayer.addListener(this@PlayerService)
+
         val hlsSource: MediaSource = HlsMediaSource.Factory(OkHttpDataSource.Factory(OkHttpClient.Builder().build()))
             .createMediaSource(MediaItem.Builder()
                 .setMimeType(MimeTypes.APPLICATION_M3U8)
@@ -53,7 +58,29 @@ class PlayerService : MediaSessionService() {
 
         exoPlayer.setMediaSource(hlsSource)
 
-        playerSession = MediaSession.Builder(this@PlayerService, exoPlayer).build()
+        forwardingExoPlayer = object : ForwardingPlayer(exoPlayer) {
+            override fun isCommandAvailable(command: Int): Boolean {
+                super.isCommandAvailable(command)
+                if (command == Player.COMMAND_SEEK_TO_PREVIOUS || command == Player.COMMAND_SEEK_TO_PREVIOUS_MEDIA_ITEM || command == Player.COMMAND_SEEK_TO_NEXT || command == Player.COMMAND_SEEK_TO_NEXT_MEDIA_ITEM) {
+                    return false
+                }
+                return super.isCommandAvailable(command)
+            }
+
+            override fun getAvailableCommands(): Player.Commands {
+                return super.getAvailableCommands()
+                    .buildUpon()
+                    .remove(COMMAND_SEEK_TO_PREVIOUS)
+                    .remove(COMMAND_SEEK_TO_PREVIOUS_MEDIA_ITEM)
+                    .remove(COMMAND_SEEK_TO_NEXT)
+                    .remove(COMMAND_SEEK_TO_NEXT_MEDIA_ITEM)
+                    .build()
+            }
+        }
+
+        playerSession = MediaSession.Builder(this@PlayerService, forwardingExoPlayer)
+            .setCallback(this@PlayerService)
+            .build()
 
         exoPlayer.playWhenReady = true
         exoPlayer.prepare()
@@ -63,9 +90,28 @@ class PlayerService : MediaSessionService() {
         return playerSession
     }
 
+    override fun onConnect(session: MediaSession, controller: MediaSession.ControllerInfo): MediaSession.ConnectionResult {
+        val sessionCommands = MediaSession.ConnectionResult.DEFAULT_SESSION_COMMANDS
+        val playerCommands = MediaSession.ConnectionResult.DEFAULT_PLAYER_COMMANDS
+            .buildUpon()
+            .remove(Player.COMMAND_SEEK_TO_PREVIOUS)
+            .remove(Player.COMMAND_SEEK_TO_PREVIOUS_MEDIA_ITEM)
+            .remove(Player.COMMAND_SEEK_TO_NEXT)
+            .remove(Player.COMMAND_SEEK_TO_NEXT_MEDIA_ITEM)
+            .build()
+
+        playerSession?.setAvailableCommands(controller, sessionCommands, playerCommands)
+        MediaSession.ConnectionResult.accept(sessionCommands, playerCommands)
+        return MediaSession.ConnectionResult.AcceptedResultBuilder(session)
+            .setAvailableSessionCommands(sessionCommands)
+            .setAvailablePlayerCommands(playerCommands)
+            .build()
+    }
+
     override fun onTaskRemoved(rootIntent: Intent?) {
         super.onTaskRemoved(rootIntent)
         exoPlayer.pause()
+        forwardingExoPlayer.pause()
         stopSelf()
     }
 
@@ -73,8 +119,11 @@ class PlayerService : MediaSessionService() {
         super.onDestroy()
         playerSession?.run {
             exoPlayer.pause()
+            forwardingExoPlayer.pause()
             exoPlayer.stop()
+            forwardingExoPlayer.stop()
             exoPlayer.release()
+            forwardingExoPlayer.release()
 
             release()
             playerSession = null

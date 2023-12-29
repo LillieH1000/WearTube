91634271f18ae7afa570923cd5d9135f5c9351e3
Lillie <91358136+LillieH1000@users.noreply.github.com>
2023-12-29 05:51:46 -0500

    Add innertube browse request

--- app/src/main/java/h/lillie/weartube/presentation/Extractor.kt
+++ app/src/main/java/h/lillie/weartube/presentation/Extractor.kt
@@ -61,6 +61,33 @@ class Extractor {
         return client.newCall(request).execute().body?.string()!!
     }
 
+    fun browseRequest(context: Context, browseID: String) : String {
+        val body: RequestBody = """{
+            "context": {
+                "client": {
+                    "hl": "en",
+                    "gl": "${context.resources.configuration.locales.get(0).country}",
+                    "clientName": "IOS",
+                    "clientVersion": "17.33.2",
+                    "deviceModel": "iPhone14,3"
+                }
+            },
+            "contentCheckOk": true,
+            "racyCheckOk": true,
+            "browseId": "$browseID"
+        }""".trimIndent().toRequestBody()
+
+        val client: OkHttpClient = OkHttpClient.Builder().build()
+
+        val request = Request.Builder()
+            .method("POST", body)
+            .header("User-Agent", "com.google.ios.youtube/17.33.2 (iPhone14,3; U; CPU iOS 15_6 like Mac OS X)")
+            .url("https://www.youtube.com/youtubei/v1/browse?key=AIzaSyB-63vPrdThhKuerbB2N_l7Kwwcxj6yUAc&prettyPrint=false")
+            .build()
+
+        return client.newCall(request).execute().body?.string()!!
+    }
+
     fun searchRequest(context: Context, query: String) : String {
         val body: RequestBody = """{
             "context": {

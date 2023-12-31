f1aa140ffe69cb8e0b84d6d9d4076aa41bd4895b
Lillie <91358136+LillieH1000@users.noreply.github.com>
2023-12-31 13:59:03 -0500

    Use custom video overlay for player controls

--- app/src/main/java/h/lillie/weartube/presentation/Player.kt
+++ app/src/main/java/h/lillie/weartube/presentation/Player.kt
@@ -3,7 +3,10 @@ package h.lillie.weartube.presentation
 import android.content.ComponentName
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import androidx.media3.common.Player
 import androidx.media3.session.MediaController
 import androidx.media3.session.SessionToken
 import androidx.media3.ui.PlayerView
@@ -11,7 +14,7 @@ import com.google.common.util.concurrent.ListenableFuture
 import com.google.common.util.concurrent.MoreExecutors
 import h.lillie.weartube.R
 
-class Player : AppCompatActivity() {
+class Player : AppCompatActivity(), Player.Listener {
     private lateinit var playerControllerFuture: ListenableFuture<MediaController>
     private lateinit var playerController: MediaController
 
@@ -19,15 +22,46 @@ class Player : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.player)
 
+        val playPauseButton: ImageButton = findViewById(R.id.playPauseButton)
+        playPauseButton.setOnClickListener {
+            if (!playerController.isPlaying) {
+                playerController.play()
+            } else {
+                playerController.pause()
+            }
+        }
+
+        val overlayView: View = findViewById(R.id.overlayView)
+        overlayView.setOnClickListener {
+            if (playPauseButton.visibility == View.GONE) {
+                overlayView.setBackgroundColor(0x40000000)
+                playPauseButton.visibility = View.VISIBLE
+            } else {
+                overlayView.setBackgroundColor(0x00000000)
+                playPauseButton.visibility = View.GONE
+            }
+        }
+
         val sessionToken = SessionToken(this@Player, ComponentName(this@Player, PlayerService::class.java))
         playerControllerFuture = MediaController.Builder(this@Player, sessionToken).buildAsync()
         playerControllerFuture.addListener({
             playerController = playerControllerFuture.get()
+            playerController.addListener(this@Player)
 
             val playerView: PlayerView = findViewById(R.id.playerView)
             playerView.keepScreenOn = true
             playerView.player = playerController
         }, MoreExecutors.directExecutor())
+    }
+
+    override fun onIsPlayingChanged(isPlaying: Boolean) {
+        super.onIsPlayingChanged(isPlaying)
+        val playPauseButton: ImageButton = findViewById(R.id.playPauseButton)
+        if (!isPlaying) {
+            playPauseButton.setImageResource(R.drawable.play)
+        } else {
+            playPauseButton.setImageResource(R.drawable.pause)
+        }
     }
 
     override fun onDestroy() {

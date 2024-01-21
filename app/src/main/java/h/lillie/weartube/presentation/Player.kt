4e049e12ca7aa7bee043c231f2ffa7d216db86ee
Lillie <91358136+LillieH1000@users.noreply.github.com>
2024-01-20 20:06:11 -0500

    Add seek bar

--- app/src/main/java/h/lillie/weartube/presentation/Player.kt
+++ app/src/main/java/h/lillie/weartube/presentation/Player.kt
@@ -3,6 +3,8 @@ package h.lillie.weartube.presentation
 import android.content.ComponentName
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
@@ -10,17 +12,23 @@ import androidx.media3.common.Player
 import androidx.media3.session.MediaController
 import androidx.media3.session.SessionToken
 import androidx.media3.ui.PlayerView
+import com.google.android.material.slider.Slider
 import com.google.common.util.concurrent.ListenableFuture
 import com.google.common.util.concurrent.MoreExecutors
 import h.lillie.weartube.R
 
 class Player : AppCompatActivity(), Player.Listener {
+    private var playerSliderHandler: Handler = Handler(Looper.getMainLooper())
+
     private lateinit var playerControllerFuture: ListenableFuture<MediaController>
     private lateinit var playerController: MediaController
+    private lateinit var playerSlider: Slider
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.player)
+
+        val deviceHeight: Int = windowManager.currentWindowMetrics.bounds.height()
 
         val playPauseButton: ImageButton = findViewById(R.id.playPauseButton)
         playPauseButton.setOnClickListener {
@@ -43,6 +51,16 @@ class Player : AppCompatActivity(), Player.Listener {
             playerController.seekForward()
         }
 
+        playerSlider = findViewById(R.id.playerSlider)
+        playerSlider.y = (deviceHeight / 2) + Converter().dpToPx(this@Player, 15f)
+        playerSlider.addOnChangeListener { _, value, fromUser ->
+            val duration: Float = playerController.duration.toFloat()
+            val position: Float = playerController.currentPosition.toFloat()
+            if (fromUser && duration >= 0 && position >= 0) {
+                playerController.seekTo(value.toLong())
+            }
+        }
+
         val overlayView: View = findViewById(R.id.overlayView)
         overlayView.setOnClickListener {
             if (playPauseButton.visibility == View.GONE) {
@@ -50,11 +68,13 @@ class Player : AppCompatActivity(), Player.Listener {
                 playPauseButton.visibility = View.VISIBLE
                 seekBackButton.visibility = View.VISIBLE
                 seekForwardButton.visibility = View.VISIBLE
+                playerSlider.visibility = View.VISIBLE
             } else {
                 overlayView.setBackgroundColor(0x00000000)
                 playPauseButton.visibility = View.GONE
                 seekBackButton.visibility = View.GONE
                 seekForwardButton.visibility = View.GONE
+                playerSlider.visibility = View.GONE
             }
         }
 
@@ -68,6 +88,11 @@ class Player : AppCompatActivity(), Player.Listener {
             playerView.keepScreenOn = true
             playerView.player = playerController
         }, MoreExecutors.directExecutor())
+    }
+
+    override fun onPlaybackStateChanged(playbackState: Int) {
+        super.onPlaybackStateChanged(playbackState)
+        playerSliderHandler.post(playerSliderTask)
     }
 
     override fun onIsPlayingChanged(isPlaying: Boolean) {
@@ -87,11 +112,26 @@ class Player : AppCompatActivity(), Player.Listener {
         playerView.keepScreenOn = false
         playerView.player = null
 
+        playerSliderHandler.removeCallbacks(playerSliderTask)
+        playerSliderHandler.removeCallbacksAndMessages(null)
+
         playerController.pause()
         playerController.stop()
         playerController.release()
 
         MediaController.releaseFuture(playerControllerFuture)
         stopService(Intent(this@Player, PlayerService::class.java))
+    }
+
+    private val playerSliderTask = object : Runnable {
+        override fun run() {
+            val duration: Float = playerController.duration.toFloat()
+            val position: Float = playerController.currentPosition.toFloat()
+            if (duration >= 0 && position >= 0 && position <= duration) {
+                playerSlider.valueTo = duration
+                playerSlider.value = position
+            }
+            playerSliderHandler.postDelayed(this, 1000)
+        }
     }
 }

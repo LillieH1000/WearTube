913aa3dd50fbbaf40753e409ebf11e3e31f2032d
Lillie <91358136+LillieH1000@users.noreply.github.com>
2024-03-15 17:12:17 -0400

    Minor type change

--- app/src/main/java/h/lillie/weartube/Player.kt
+++ app/src/main/java/h/lillie/weartube/Player.kt
@@ -68,7 +68,7 @@ class Player : AppCompatActivity(), Player.Listener {
         bluetoothButton.y = Converter().dpToPx(this@Player, -50f)
         bluetoothButton.setOnClickListener {
             playerController.pause()
-            val intent: Intent = Intent(Settings.ACTION_BLUETOOTH_SETTINGS)
+            val intent = Intent(Settings.ACTION_BLUETOOTH_SETTINGS)
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
             intent.putExtra("EXTRA_CONNECTION_ONLY", true)
             intent.putExtra("EXTRA_CLOSE_ON_CONNECT", true)
